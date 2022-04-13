c7c02f811f42322fd70efe74e6eb3b6d6a8550b1
Jojo <example@gmail.com>
2022-04-13 18:21:05 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"Use strick";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+const myFn = (a,b) => {
+    let c = 10;
+    c = a + b;
+};
+
+const array = [1,2,3,4,5];
+
+array.push(true);
+
+console.log(array);
+
+
