340ea97976d12b8bcdd7d6166ddc98a7b68d565d
Jojo <example@gmail.com>
2022-04-30 19:50:42 +0300

    deleted function

--- function.js
+++
@@ -1,9 +0,0 @@
-const myFn = (a,b) => {
-    let c = 20;
-    a = 10;
-    b = 10;
-    c = a * b;
-    return c;
-};
-
-myFn();
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"Use strick";
+"Use strict";
 
 let number = 5;
 const leftBorderWidth = 1;
@@ -15,14 +15,33 @@ obj.a = 10;
 console.log(obj);
 
 const myFn = (a,b) => {
-    let c = 10;
+    let c;
     c = a + b;
+    return c;
 };
+
+console.log(myFn(10,10));
 
 const array = [1,2,3,4,5];
 
 array.push(true);
 
 console.log(array);
+
+const userName = 'DronkKibenbl4';
+
+// alert(`Hello, ${userName}`);
+
+let incr = 10,
+    decr = 5;
+
+    console.log(++incr);
+
+const fls = 0;
+const tre = 1;
+
+
+console.log(!fls);
+
 
 
